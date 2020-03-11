3fc009406e5c0a90c420d3ee03843e2d9148c900
Dmitry <dmitryorly611@gmail.com>
2020-03-11 16:23:04 +0300

    Start to improve SELECT

--- scss/blocks/select/select.js
+++ scss/blocks/select/select.js
@@ -1,3 +1,10 @@
 const select = document.getElementsByClassName('select__container')[0];
-console.log(select);
-select.addEventListener('click', () => select.classList.toggle('select__container_checked'));
+let activeOption = document.getElementsByClassName('select__option_active')[0];
+const options = document.getElementsByClassName('select__option');
+
+// Drop-down
+select.addEventListener('click', function () {
+  select.classList.toggle('select__container_checked');
+  select.after.style.transform = 'rotate(180deg)';
+});
+
