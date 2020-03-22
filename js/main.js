34649a0ab260184447ccead38951836868f51bb9
Dmitry <dmitryorly611@gmail.com>
2020-03-22 18:16:06 +0300

    Close menu by clicking on links

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
+// Burger menu
 (function () {
   let burgerClose = document.querySelector('.mobile__close');
   let burgerOpen = document.querySelector('.header__burger');
   let menu = document.querySelector('.mobile__menu');
+  let menuItems = document.querySelectorAll(".mobile-menu__link");
+  
+  for (let i = 0; i < menuItems.length; i++) {
+    menuItems[i].addEventListener('click', () => {
+      menu.classList.remove('mobile__menu_active');
+    });
+  }
 
   burgerClose.addEventListener('click', () => {
     menu.classList.remove('mobile__menu_active');
