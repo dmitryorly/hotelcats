a68f3d6a7f2abc17768b0d215df73ded9945aa2d
Dmitry <dmitryorly611@gmail.com>
2020-04-03 17:58:33 +0300

    Mobile slider v1
    - does not work in firefox!
    - controls by swipes
    - has a blind spot 35px for vertical scroll
    - has an issue with indicators hover effect

--- js/slider.js
+++ js/slider.js
@@ -1,11 +1,50 @@
 function sliderInit(mainElemSelector) {
   
-  const slider = document.querySelector(mainElemSelector),
+  const slider = document.querySelector(mainElemSelector), 
+      slideContainer = slider.querySelector('.slider__slide-container'),
       indicators = slider.querySelectorAll(".indicator"),
       slides = slider.querySelectorAll(".slide"),
       nextButton = slider.querySelector(".next-button"),
       prevButton = slider.querySelector(".prev-button");
 let activeIndex = 0;
+
+const nextSlide = function () {
+  indicators[activeIndex].classList.remove('indicator_active');
+  activeIndex < slides.length - 1 ? activeIndex += 1 : activeIndex = 0;
+  indicators[activeIndex].classList.add('indicator_active');
+  slides.forEach(slide => translate(slide, activeIndex))
+}
+
+const prevSlide = function () {
+  indicators[activeIndex].classList.remove('indicator_active');
+  activeIndex >= 1 ? activeIndex -= 1 : activeIndex = slides.length - 1;
+  indicators[activeIndex].classList.add('indicator_active');
+  slides.forEach(slide => translate(slide, activeIndex))
+}
+
+//mobile start
+
+slideContainer.addEventListener('touchstart', lock)
+slideContainer.addEventListener('touchend', move)
+
+let posX = null;
+
+function unify(e) { return e.changedTouches ? e.changedTouches[0] : e };
+
+function lock (ev) {
+  posX = unify(ev).clientX;
+  console.log( posX );
+}
+
+function move (ev) {
+  if ( posX - unify(ev).clientX > 35 ) {
+    nextSlide();
+  } else if ( posX - unify(ev).clientX < - 35 ) {
+    prevSlide();
+  } else return
+}
+
+//mobile end
 
 const translate = (slide, index = activeIndex) => {
   return slide.style.transform = 'translateX(' + -100 * activeIndex + '%)'
@@ -18,21 +57,11 @@ indicators.forEach((indicator, index) => indicator.addEventListener('click', () 
   slides.forEach(slide => translate(slide, index))
 }))
 
-prevButton.addEventListener('click', () =>{
-  indicators[activeIndex].classList.remove('indicator_active');
-  activeIndex >= 1 ? activeIndex -= 1 : activeIndex = slides.length - 1;
-  indicators[activeIndex].classList.add('indicator_active');
-  slides.forEach(slide => translate(slide, activeIndex))
-} )
+prevButton.addEventListener('click', prevSlide )
 
-nextButton.addEventListener('click', () =>{
-  indicators[activeIndex].classList.remove('indicator_active');
-  activeIndex < slides.length - 1 ? activeIndex += 1 : activeIndex = 0;
-  indicators[activeIndex].classList.add('indicator_active');
-  slides.forEach(slide => translate(slide, activeIndex))
-} )
+nextButton.addEventListener( 'click', nextSlide )
   
 }
 
-(sliderInit("#review-slider"))
-(sliderInit("#rooms-slider"))
+sliderInit("#review-slider")
+sliderInit("#rooms-slider")
